36e9733e488cddde67d23251b7a308ae2d9074ba
ccristopher <crist.r.enc@gmail.com>
2023-09-16 17:43:44 -0400

    Added some old files

---
+++ Module 01/Mod01 Assignments/01.04 Student Information/StudentInfoCard.java
@@ -0,0 +1,34 @@
+
+/**
+ * a program to print information found on a Student Information Card
+ *
+ * @Cristopher Encarnacion
+ * @September 14, 2023
+ */
+public class StudentInfoCard
+{
+    public static void main(String[] args)
+    {
+       System.out.println("*****************************************************************************");
+       System.out.println("*                                                                           *");
+       System.out.println("*                            Student Information                            *");
+       System.out.println("*                                                                           *");
+       System.out.println("*****************************************************************************");
+       System.out.println();
+       System.out.print("Name: Cristopher Encarnacion        ");
+       System.out.println("Birthday: March 15, 2006");
+       System.out.print("Age: 17                             ");
+       System.out.println("Grade: 12th");
+       System.out.print("School: Miami Central Senior High   ");
+       System.out.println("City: Miami, FL");
+       System.out.println();
+       System.out.print("Cell Phone: 786-464-6806   ");
+       System.out.println("Available: 3:00pm-9:00pm Mon-Fri");
+       System.out.println("Email: crist.r.enc@gmail.com");
+       System.out.println();
+       System.out.println("Mom's contact information");
+       System.out.print("Cell Phone: 786-690-0199   ");
+       System.out.println("Available: Anytime");
+       System.out.println("Email: claudine875@gmail.com");
+    }
+}
---
+++ Module 01/Mod01 Lessons/01.04 HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * code to output text
+ *
+ * @Cristopher Encarnacion
+ * @September 14, 2023
+ */
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.print("Hello, Virtual World!");
+        System.out.println();
+        System.out.println("It is a great day for programming.");
+    }
+}
