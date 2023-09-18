7e63d33842505e6c24ae9476f679d8b6eed88dbf
ccristopher <crist.r.enc@gmail.com>
2023-09-18 09:21:22 -0400

    Fixed Calc.java, removed hardcoded values

--- Module 01/Mod01 Assignments/_01.14 DBA/src/Calc.java
+++ Module 01/Mod01 Assignments/_01.14 DBA/src/Calc.java
@@ -2,11 +2,12 @@ public class Calc
 {
     public static void main(String[] args)
     {
-        int value = 7;
+        int value1 = 7;
+        int value2 = 2;
         //calculation and output statements
-        System.out.println("The original value is " + value + ".");
-        System.out.println("When " + value + " is divided by 2, the whole number result is " + value / 2 + ".");
-        System.out.println("The remainder of " + value + " divided by 2 is " + value % 2 + ".");
-        System.out.print("When " + value + " is divided by 2, the decimal result is " + value / 2.0 + ".");
+        System.out.println("The original value is " + value1 + ".");
+        System.out.println("When " + value1 + " is divided by " + value2 + ", the whole number result is " + value1 / value2 + ".");
+        System.out.println("The remainder of " + value1 + " divided by " + value2 + " is " + value1 % value2 + ".");
+        System.out.print("When " + value1 + " is divided by " + value2 + ", the decimal result is " + (double) value1 / value2 + ".");
     }
 }
