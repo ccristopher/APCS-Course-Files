ff1983080b7e429c4ce54c90c6ffce69d04339de
ccristopher <crist.r.enc@gmail.com>
2023-09-20 16:20:52 -0400

    my masterpiece is complete

--- Module 02/Mod02 Assignments/_02.10 Online Purchase/src/BuyMovieTickets.java
+++ Module 02/Mod02 Assignments/_02.10 Online Purchase/src/BuyMovieTickets.java
@@ -3,48 +3,56 @@ public class BuyMovieTickets
 {
     public static void main(String[] args)
     {
-        // declare local variables
+        // create scanner
         Scanner in = new Scanner(System.in);
 
-
+        // collect info
         System.out.print("Please enter your first and last name: ");
         String firstName = in.next();
-        String firstInitial = firstName.charAt(0) + ". ";
-        System.out.println(firstInitial);
         String lastName = in.nextLine().substring(1);
-        System.out.println(lastName);
-        System.out.println(firstName);
 
         System.out.print("Enter todays date (mm/dd/yyyy): ");
-        String date = in.next().replace('/', '-') + in.nextLine();
-        System.out.println(date);
+        String date = in.next().replace('/', '-'); // reformat date
+        in.nextLine();
 
         System.out.print("What movie are you here to see?: ");
         String movie = in.next() + in.nextLine();
-        System.out.println(movie);
 
         System.out.print("How many tickets?: ");
         int tickets = Integer.parseInt(in.next());
         in.nextLine();
-        System.out.println(tickets);
 
         System.out.print("What is the price of the ticket?: $");
         double price = Double.parseDouble(in.next());
         in.nextLine();
-        System.out.println(price);
 
-        System.out.print("Debit card (#####-####-###): ");
+        System.out.print("Debit card (#####-###-####): ");
         String card = in.next();
-        String censoredCard = "#####-####-" + card.substring(11,14);
-        System.out.println(censoredCard);
+        in.nextLine();
 
         System.out.print("PIN (#####): ");
         String pin = in.next();
         in.nextLine();
-        System.out.print(pin);
 
+        // quick calculations
+        String order = lastName.substring(1, 3) + card.substring(6, 9); // create order number
+        char firstInitial = firstName.charAt(0); // censor first name
+        String censoredCard = "#####-###-" + card.substring(10, 14); // censor debit card
+        double totalCost = tickets * price; // calculate total cost
         String column = "*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~";
-        System.out.println("\n\n" + column + "\n" + column);
 
+        // print receipt
+        System.out.println("\n\n" + column + "\n" + column);
+        System.out.println("Your e-Receipt");
+        System.out.println("\n" + date);
+        System.out.println("Order number: " + order);
+        System.out.println("\n\t" + firstInitial + ". " + lastName);
+        System.out.println("\tAccount: " + censoredCard);
+        System.out.println("\tMovie: " + movie);
+        System.out.println("\tNumber of Tickets: " + tickets);
+        System.out.println("\tTicket Price: " + price);
+        System.out.println("\n\t$" + totalCost + " will be debited to your account.");
+        System.out.println("\n\tThank you!");
+        System.out.println(column + "\n" + column);
     }
 }
